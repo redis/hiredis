0b27639e0974df09e631cafebc46e43cf1a2f018
Pieter Noordhuis <pcnoordhuis@gmail.com>
2010-11-15 20:17:31 +0100

    Don't print when testing can continue

--- test.c
+++ test.c
@@ -91,12 +91,9 @@ static void test_blocking_connection() {
 
     /* Make sure the DB is emtpy */
     reply = redisCommand(c,"DBSIZE");
-    if (reply->type != REDIS_REPLY_INTEGER ||
-        reply->integer != 0) {
-        printf("Sorry DB 9 is not empty, test can not continue\n");
+    if (reply->type != REDIS_REPLY_INTEGER || reply->integer != 0) {
+        printf("Database #9 is not empty, test can not continue\n");
         exit(1);
-    } else {
-        printf("DB 9 is empty... test can continue\n");
     }
     freeReplyObject(reply);
 
