3d76f3fe02c8fa4eac1bb2fb266897f69f159ee8
Pieter Noordhuis <pcnoordhuis@gmail.com>
2010-12-28 17:59:26 +0100

    Add write event after setting connect callback

--- async.c
+++ async.c
@@ -96,6 +96,11 @@ int redisAsyncSetReplyObjectFunctions(redisAsyncContext *ac, redisReplyObjectFun
 int redisAsyncSetConnectCallback(redisAsyncContext *ac, redisConnectCallback *fn) {
     if (ac->onConnect == NULL) {
         ac->onConnect = fn;
+
+        /* The common way to detect an established connection is to wait for
+         * the first write event to be fired. This assumes the related event
+         * library functions are already set. */
+        if (ac->evAddWrite) ac->evAddWrite(ac->_adapter_data);
         return REDIS_OK;
     }
     return REDIS_ERR;
