5962a2de17b274a9313e87f33fbc7ba17d1dbd83
Pieter Noordhuis <pcnoordhuis@gmail.com>
2011-01-19 13:38:47 +0100

    Add testhelp.h inline in sds.c

--- sds.c
+++ sds.c
@@ -502,7 +502,20 @@ err:
 
 #ifdef SDS_TEST_MAIN
 #include <stdio.h>
-#include "testhelp.h"
+
+int __failed_tests = 0;
+int __test_num = 0;
+#define test_cond(descr,_c) do { \
+    __test_num++; printf("%d - %s: ", __test_num, descr); \
+    if(_c) printf("PASSED\n"); else {printf("FAILED\n"); __failed_tests++;} \
+} while(0);
+#define test_report() do { \
+    printf("%d tests, %d passed, %d failed\n", __test_num, \
+                    __test_num-__failed_tests, __failed_tests); \
+    if (__failed_tests) { \
+        printf("=== WARNING === We have failed tests here...\n"); \
+    } \
+} while(0);
 
 int main(void) {
     {
