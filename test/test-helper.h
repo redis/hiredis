bb2d09113cb6c14eba6a2d4c97713184d874274e
Pieter Noordhuis <pcnoordhuis@gmail.com>
2011-09-30 14:28:34 +0200

    Move custom asserts to test helper

---
+++ test/test-helper.h
@@ -0,0 +1,35 @@
+#ifndef _TEST_HELPER_H
+#define _TEST_HELPER_H 1
+
+#include <stdio.h>
+#include <assert.h>
+
+#define assert_equal(a, b, type, fmt) do {       \
+    type a_ = (a);                               \
+    type b_ = (b);                               \
+    if (a_ != b_) {                              \
+        fprintf(stderr,                          \
+            "%s:%d: " fmt " != " fmt "\n",       \
+            __FILE__, __LINE__, a_, b_);         \
+        assert(0);                               \
+    }                                            \
+} while(0)
+
+#define assert_equal_int(a,b) do {               \
+    assert_equal(a, b, int, "%d");               \
+} while(0)
+
+#define assert_equal_size_t(a,b) do {            \
+    assert_equal(a, b, size_t, "%lu");           \
+} while(0)
+
+/* Use long long to avoid compiler warnings about the printf format. */
+#define assert_equal_int64_t(a,b) do {           \
+    assert_equal(a, b, long long, "%lld");       \
+} while(0)
+
+#define assert_equal_double(a,b) do {            \
+    assert_equal(a, b, double, "%f");            \
+} while(0)
+
+#endif
--- test/test-parser.c
+++ test/test-parser.c
@@ -4,40 +4,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <assert.h>
 #include <errno.h>
 
 /* local */
+#include "test-helper.h"
 #include "../parser.h"
 #include "../sds.h"
-
-#define assert_equal(a, b, type, fmt) do {       \
-    type a_ = (a);                               \
-    type b_ = (b);                               \
-    if (a_ != b_) {                              \
-        fprintf(stderr,                          \
-            "%s:%d: " fmt " != " fmt "\n",       \
-            __FILE__, __LINE__, a_, b_);         \
-        assert(0);                               \
-    }                                            \
-} while(0)
-
-#define assert_equal_int(a,b) do {               \
-    assert_equal(a, b, int, "%d");               \
-} while(0)
-
-#define assert_equal_size_t(a,b) do {            \
-    assert_equal(a, b, size_t, "%lu");           \
-} while(0)
-
-/* Use long long to avoid compiler warnings about the printf format. */
-#define assert_equal_int64_t(a,b) do {           \
-    assert_equal(a, b, long long, "%lld");       \
-} while(0)
-
-#define assert_equal_double(a,b) do {            \
-    assert_equal(a, b, double, "%f");            \
-} while(0)
 
 typedef struct log_entry_s log_entry_t;
 
