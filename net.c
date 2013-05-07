637f54b92c9b373b74cbab34fb89a583cebc54c0
Eugene Bolotin <eugenebolotin@gmail.com>
2013-05-07 18:26:05 +0400

    Fix possible uninitialized value access due to strerror_r error

--- net.c
+++ net.c
@@ -55,7 +55,7 @@
 void __redisSetError(redisContext *c, int type, const char *str);
 
 static void __redisSetErrorFromErrno(redisContext *c, int type, const char *prefix) {
-    char buf[128];
+    char buf[128] = { 0 };
     size_t len = 0;
 
     if (prefix != NULL)
