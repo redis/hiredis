90c8df1dc1366e6417c40616cd044729cee98d49
ripcurld00d <boazyeah@gmail.com>
2013-07-29 19:35:48 +0300

    Fix "host cannot be resolved test" test

--- test.c
+++ test.c
@@ -293,6 +293,7 @@ static void test_blocking_connection_errors(void) {
         (strcmp(c->errstr,"Name or service not known") == 0 ||
          strcmp(c->errstr,"Can't resolve: idontexist.local") == 0 ||
          strcmp(c->errstr,"nodename nor servname provided, or not known") == 0 ||
+         strcmp(c->errstr,"No address associated with hostname") == 0 ||
          strcmp(c->errstr,"no address associated with name") == 0));
     redisFree(c);
 
