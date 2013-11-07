7514b0b2933c7fa510d7859d62856a5514fc6002
John Graham <john@simpli.fi>
2013-11-06 20:53:36 -0600

    Removed warning

--- adapters/libuv.h
+++ adapters/libuv.h
@@ -11,6 +11,7 @@ typedef struct redisLibuvEvents {
   int                events;
 } redisLibuvEvents;
 
+int redisLibuvAttach(redisAsyncContext*, uv_loop_t*);
 
 static void redisLibuvPoll(uv_poll_t* handle, int status, int events) {
   redisLibuvEvents* p = (redisLibuvEvents*)handle->data;
