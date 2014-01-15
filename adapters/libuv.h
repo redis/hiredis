1e9ffae027ec594306fc6fb903d05f66ef57e4bf
Yorkie <l900422@vip.qq.com>
2014-01-15 19:40:34 +0800

    fix duplicate symbol error

--- adapters/libuv.h
+++ adapters/libuv.h
@@ -12,109 +12,26 @@ typedef struct redisLibuvEvents {
 } redisLibuvEvents;
 
 
-static void redisLibuvPoll(uv_poll_t* handle, int status, int events) {
-  redisLibuvEvents* p = (redisLibuvEvents*)handle->data;
-
-  if (status != 0) {
-    return;
-  }
-
-  if (events & UV_READABLE) {
-    redisAsyncHandleRead(p->context);
-  }
-  if (events & UV_WRITABLE) {
-    redisAsyncHandleWrite(p->context);
-  }
-}
+static void redisLibuvPoll(uv_poll_t* handle, int status, int events);
 
 
-static void redisLibuvAddRead(void *privdata) {
-  redisLibuvEvents* p = (redisLibuvEvents*)privdata;
-
-  p->events |= UV_READABLE;
-
-  uv_poll_start(&p->handle, p->events, redisLibuvPoll);
-}
+static void redisLibuvAddRead(void *privdata);
 
 
-static void redisLibuvDelRead(void *privdata) {
-  redisLibuvEvents* p = (redisLibuvEvents*)privdata;
-
-  p->events &= ~UV_READABLE;
-
-  if (p->events) {
-    uv_poll_start(&p->handle, p->events, redisLibuvPoll);
-  } else {
-    uv_poll_stop(&p->handle);
-  }
-}
+static void redisLibuvDelRead(void *privdata);
 
 
-static void redisLibuvAddWrite(void *privdata) {
-  redisLibuvEvents* p = (redisLibuvEvents*)privdata;
-
-  p->events |= UV_WRITABLE;
-
-  uv_poll_start(&p->handle, p->events, redisLibuvPoll);
-}
+static void redisLibuvAddWrite(void *privdata);
 
 
-static void redisLibuvDelWrite(void *privdata) {
-  redisLibuvEvents* p = (redisLibuvEvents*)privdata;
-
-  p->events &= ~UV_WRITABLE;
-
-  if (p->events) {
-    uv_poll_start(&p->handle, p->events, redisLibuvPoll);
-  } else {
-    uv_poll_stop(&p->handle);
-  }
-}
+static void redisLibuvDelWrite(void *privdata);
 
 
-static void on_close(uv_handle_t* handle) {
-  redisLibuvEvents* p = (redisLibuvEvents*)handle->data;
-
-  free(p);
-}
+static void on_close(uv_handle_t* handle);
 
 
-static void redisLibuvCleanup(void *privdata) {
-  redisLibuvEvents* p = (redisLibuvEvents*)privdata;
-
-  uv_close((uv_handle_t*)&p->handle, on_close);
-}
+static void redisLibuvCleanup(void *privdata);
 
 
-int redisLibuvAttach(redisAsyncContext* ac, uv_loop_t* loop) {
-  redisContext *c = &(ac->c);
-
-  if (ac->ev.data != NULL) {
-    return REDIS_ERR;
-  }
-
-  ac->ev.addRead  = redisLibuvAddRead;
-  ac->ev.delRead  = redisLibuvDelRead;
-  ac->ev.addWrite = redisLibuvAddWrite;
-  ac->ev.delWrite = redisLibuvDelWrite;
-  ac->ev.cleanup  = redisLibuvCleanup;
-
-  redisLibuvEvents* p = malloc(sizeof(*p));
-
-  if (!p) {
-    return REDIS_ERR;
-  }
-
-  memset(p, 0, sizeof(*p));
-
-  if (uv_poll_init(loop, &p->handle, c->fd) != 0) {
-    return REDIS_ERR;
-  }
-
-  ac->ev.data    = p;
-  p->handle.data = p;
-  p->context     = ac;
-
-  return REDIS_OK;
-}
+int redisLibuvAttach(redisAsyncContext* ac, uv_loop_t* loop);
 #endif
