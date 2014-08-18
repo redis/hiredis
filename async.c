8fa725f674f9de275568cb793ff37225f70c1b9d
Hang Su <hangsu@yahoo-inc.com>
2014-08-18 22:33:48 +0000

    remove the explicit type conversion, after changing args to const char *

--- async.c
+++ async.c
@@ -661,6 +661,6 @@ int redisAsyncCommandArgv(redisAsyncContext *ac, redisCallbackFn *fn, void *priv
 }
 
 int redisAsyncFormattedCommand(redisAsyncContext *ac, redisCallbackFn *fn, void *privdata, const char *cmd, size_t len) {
-    int status = __redisAsyncCommand(ac,fn,privdata,(char *)cmd,len);
+    int status = __redisAsyncCommand(ac,fn,privdata,cmd,len);
     return status;
 }
