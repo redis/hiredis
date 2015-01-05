9b83ddc2d99ffc2b650e7efc9e0ed82ecb37fdf3
Matt Stancliff <matt@genges.com>
2015-01-05 10:22:22 -0500

    Fix clang analyzer warning
    redisAsyncInitialize() can return NULL, but then we pass
    the return value from redisAsyncInitialize() into something
    dereferencing the return value, which can cause crashies.

--- async.c
+++ async.c
@@ -142,6 +142,9 @@ static redisAsyncContext *redisAsyncInitialize(redisContext *c) {
 /* We want the error field to be accessible directly instead of requiring
  * an indirection to the redisContext struct. */
 static void __redisAsyncCopyError(redisAsyncContext *ac) {
+    if (!ac)
+        return;
+
     redisContext *c = &(ac->c);
     ac->err = c->err;
     ac->errstr = c->errstr;
