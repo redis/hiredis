386b9950f3b72df0cf616470ea4fb2d1fe24b23a
ShooterIT <wangyuancode@163.com>
2020-01-01 14:42:10 +0800

    fix spelling mistakes

--- hiredis.h
+++ hiredis.h
@@ -101,7 +101,7 @@ typedef struct redisReply {
     double dval; /* The double when type is REDIS_REPLY_DOUBLE */
     size_t len; /* Length of string */
     char *str; /* Used for REDIS_REPLY_ERROR, REDIS_REPLY_STRING
-                  and REDIS_REPLY_DOUBLE (in additionl to dval). */
+                  and REDIS_REPLY_DOUBLE (in additional to dval). */
     size_t elements; /* number of elements, for REDIS_REPLY_ARRAY */
     struct redisReply **element; /* elements vector for REDIS_REPLY_ARRAY */
 } redisReply;
