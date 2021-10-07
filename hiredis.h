076241a05ee142b3a15082209677b516f1dbcba9
Paul Fee <83710470+jfeep@users.noreply.github.com>
2021-10-07 17:13:37 +0100

    Maintain ABI compatibility with 1.0.0

--- hiredis.h
+++ hiredis.h
@@ -48,7 +48,7 @@ typedef long long ssize_t;
 #define HIREDIS_MAJOR 1
 #define HIREDIS_MINOR 0
 #define HIREDIS_PATCH 2
-#define HIREDIS_SONAME 1.0.2-dev
+#define HIREDIS_SONAME 1.0.0
 
 /* Connection type can be blocking or non-blocking and is set in the
  * least significant bit of the flags field in redisContext. */
