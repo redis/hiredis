e9c245c5437f3ca584755fda8576c15169b7e9f4
sunmingqi <sunmingqi@corp.netease.com>
2021-11-03 22:21:18 +0800

    add timeout support for libuv adapter

--- adapters/libuv.h
+++ adapters/libuv.h
@@ -9,6 +9,7 @@
 typedef struct redisLibuvEvents {
   redisAsyncContext* context;
   uv_poll_t          handle;
+  uv_timer_t         timer;
   int                events;
 } redisLibuvEvents;
 
@@ -69,19 +70,55 @@ static void redisLibuvDelWrite(void *privdata) {
   }
 }
 
+static void on_timer_close(uv_handle_t *handle) {
+  // a dummy value, make sure callback is sequenced
+  handle->data = (void *)0xDEADBEEF;
+}
 
-static void on_close(uv_handle_t* handle) {
+static void on_handle_close(uv_handle_t *handle) {
   redisLibuvEvents* p = (redisLibuvEvents*)handle->data;
-
+  // note that the close callbacks is called sequentially
+  // so on_timer_close is always called before on_handle_close
+  assert(p->timer.data == (void *)0xDEADBEEF);
+  // just make sure callback is sequenced
   hi_free(p);
 }
 
+static void redisLibuvTimeout(uv_timer_t *timer) {
+  redisLibuvEvents *e = (redisLibuvEvents*)timer->data;
+  redisAsyncHandleTimeout(e->context);
+}
+
+static void redisLibuvSetTimeout(void *privdata, struct timeval tv) {
+  redisLibuvEvents* p = (redisLibuvEvents*)privdata;
+
+  uint64_t millsec = tv.tv_sec * 1000 + tv.tv_usec / 1000.0;
+  if (p->timer.type == UV_UNKNOWN_HANDLE) {
+    // timer is uninitialized
+    if (uv_timer_init(p->handle.loop, &p->timer) != 0) {
+      return;
+    }
+    p->timer.data = p;
+  }
+  // updates the timeout if the timer has already started
+  // or start the timer
+  uv_timer_start(&p->timer, redisLibuvTimeout, millsec, 0);
+}
 
 static void redisLibuvCleanup(void *privdata) {
   redisLibuvEvents* p = (redisLibuvEvents*)privdata;
 
   p->context = NULL; // indicate that context might no longer exist
-  uv_close((uv_handle_t*)&p->handle, on_close);
+  if (p->timer.type != UV_UNKNOWN_HANDLE) {
+    // note that the close callbacks is called sequentially
+    // so `on_timer_close` is always called before `on_handle_close`
+    // `uv_close` will stop the timer internally
+    uv_close((uv_handle_t*)&p->timer, on_timer_close);
+  } else {
+    p->timer.data = (void*)0xDEADBEEF;
+    // a dummy value, make sure callback is sequenced
+  }
+  uv_close((uv_handle_t*)&p->handle, on_handle_close);
 }
 
 
@@ -92,11 +129,12 @@ static int redisLibuvAttach(redisAsyncContext* ac, uv_loop_t* loop) {
     return REDIS_ERR;
   }
 
-  ac->ev.addRead  = redisLibuvAddRead;
-  ac->ev.delRead  = redisLibuvDelRead;
-  ac->ev.addWrite = redisLibuvAddWrite;
-  ac->ev.delWrite = redisLibuvDelWrite;
-  ac->ev.cleanup  = redisLibuvCleanup;
+  ac->ev.addRead        = redisLibuvAddRead;
+  ac->ev.delRead        = redisLibuvDelRead;
+  ac->ev.addWrite       = redisLibuvAddWrite;
+  ac->ev.delWrite       = redisLibuvDelWrite;
+  ac->ev.cleanup        = redisLibuvCleanup;
+  ac->ev.scheduleTimer  = redisLibuvSetTimeout;
 
   redisLibuvEvents* p = (redisLibuvEvents*)hi_malloc(sizeof(*p));
   if (p == NULL)
