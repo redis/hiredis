1539b7d0b7a0a152b4465a96d41df19ea8ba1b6f
sunmingqi <sunmingqi@corp.netease.com>
2022-01-14 13:58:19 +0800

    remove outdated comments

--- adapters/libuv.h
+++ adapters/libuv.h
@@ -92,14 +92,6 @@ static void on_handle_close(uv_handle_t *handle) {
 
 // libuv removed `status` parameter since v0.11.23
 // see: https://github.com/libuv/libuv/blob/v0.11.23/include/uv.h
-// explain:
-// 1. !defined(UV_VERSION_PATCH), must < 0.10.2
-// 2. major.minor.patch < 0.11.23
-// => (major < 0) || (major == 0 && minor < 11) || (minor == 11 && patch < 23)
-//    major < 0 can't happen
-// => (major == 0 && minor < 11) || (minor == 11 && patch < 23)
-
-//          < 0.10.2 goes here                  < 0.10.2~x goes here                                     0.11.0~22 goes here
 #if (UV_VERSION_MAJOR == 0 && UV_VERSION_MINOR < 11) || \
     (UV_VERSION_MAJOR == 0 && UV_VERSION_MINOR == 11 && UV_VERSION_PATCH < 23)
 static void redisLibuvTimeout(uv_timer_t *timer, int status) {
