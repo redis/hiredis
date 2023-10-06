a1f45c6334a6fe128d504416e3a49603a331547f
timaty the 8 <101657712+kendamaskareivis@users.noreply.github.com>
2023-10-06 13:41:36 +0300

    Update hiredis.h
    Removed dev

--- hiredis.h
+++ hiredis.h
@@ -48,7 +48,7 @@ typedef long long ssize_t;
 #define HIREDIS_MAJOR 1
 #define HIREDIS_MINOR 2
 #define HIREDIS_PATCH 0
-#define HIREDIS_SONAME 1.2.1-dev
+#define HIREDIS_SONAME 1
 
 /* Connection type can be blocking or non-blocking and is set in the
  * least significant bit of the flags field in redisContext. */
