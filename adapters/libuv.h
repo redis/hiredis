2706c3e16d57c9babd24bfd83ade2efcdb13a7b8
Mark Agranat <agranatmarkit@gmail.com>
2023-11-14 23:53:25 +0100

    Fix memory leak.
    When redisLibuvAttach receives error from call to
    uv_poll_init_socket there is a memory leaked ptr
    of type redisLibuvEvents.

--- adapters/libuv.h
+++ adapters/libuv.h
@@ -159,6 +159,7 @@ static int redisLibuvAttach(redisAsyncContext* ac, uv_loop_t* loop) {
     memset(p, 0, sizeof(*p));
 
     if (uv_poll_init_socket(loop, &p->handle, c->fd) != 0) {
+        hi_free(p);
         return REDIS_ERR;
     }
 
