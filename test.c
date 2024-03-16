7ab6b824c771df8298cbe1b512a80116d005ec33
guoguangwu <guoguangwug@gmail.com>
2024-03-16 09:20:31 +0800

    fix: typos
    Signed-off-by: guoguangwu <guoguangwug@gmail.com>

--- test.c
+++ test.c
@@ -156,7 +156,7 @@ static redisContext *select_database(redisContext *c) {
     assert(reply != NULL);
     freeReplyObject(reply);
 
-    /* Make sure the DB is emtpy */
+    /* Make sure the DB is empty */
     reply = redisCommand(c,"DBSIZE");
     assert(reply != NULL);
     if (reply->type == REDIS_REPLY_INTEGER && reply->integer == 0) {
@@ -2298,7 +2298,7 @@ static void test_async_polling(struct config config) {
      */
     test("Ping/Pong from onConnected callback (Issue #931): ");
     c = do_aconnect(config, ASTEST_ISSUE_931_PING);
-    /* connect callback issues ping, reponse callback destroys context */
+    /* connect callback issues ping, response callback destroys context */
     while(astest.ac)
         redisPollTick(c, 0.1);
     assert(astest.connected == 0);
